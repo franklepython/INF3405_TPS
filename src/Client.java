2848dca93114c4cd04d70b8a645460b0b2fdcd00
Francis Trahan <francis.trahan@polymtl.ca>
2024-01-15 19:29:26 -0500

    Pu d erreur de syntaxe

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,7 @@
 import java.io.DataInputStream;
 import java.net.Socket;
-// Application client
-public class Client {
+	// Application client
+	public class Client {
 	private static Socket socket;
 	public static void main(String[] args) throws Exception {
 	// Adresse et port du serveur
@@ -13,7 +13,7 @@ public class Client {
 	// Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
 	DataInputStream in = new DataInputStream(socket.getInputStream());
 	// Attente de la réception d'un message envoyé par le, server sur le canal
-	String helloMessageFromServer = in.readUTF();
+		String helloMessageFromServer = in.readUTF();
 	System.out.println(helloMessageFromServer);
 	// fermeture de La connexion avec le serveur
 	socket.close();
